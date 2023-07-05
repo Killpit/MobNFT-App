3aa5ae41f7a575c3ccfbdc594c1e56c1db81fc27
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-05 14:00:07 +0000

    5 Tem 2023 17:00

--- mobile_nft/lib/utils/config.dart
+++ mobile_nft/lib/utils/config.dart
@@ -19,7 +19,7 @@ const pinataUrl = "https://api.pinata.cloud";
 const pinEndpoint = "/pinning/pinFileToIPFS";
 const apiKey = "f83f67660f94493c49c5";
 const apiSecret =
-    "220f40e9f0e538ba2e04f09747057bdd679bbe849a56c64de6158dc1a96bcdfc";
+    "";
 const jwt =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJlNTZhODJiNi1iYmY5LTRiNDgtYmViNy0wNTA4NzY1OGRjYjkiLCJlbWFpbCI6Im1vYnRlcmVzdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiZjgzZjY3NjYwZjk0NDkzYzQ5YzUiLCJzY29wZWRLZXlTZWNyZXQiOiIyMjBmNDBlOWYwZTUzOGJhMmUwNGYwOTc0NzA1N2JkZDY3OWJiZTg0OWE1NmM2NGRlNjE1OGRjMWE5NmJjZGZjIiwiaWF0IjoxNjgwMTgzODc2fQ.Z8RDxTTFzDuMsEOMWpDWExXS8uAyy0OqqF15xeSnn_A";
+    "";
 const pinataGateway = "https://gateway.pinata.cloud/ipfs/";
