e51f79d49eda15e1f39c70b00d80ae01214a1b5f
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-05 14:35:03 +0000

    5 Tem 2023 17:35

---
+++ mobile_nft/lib/features/CreateArticles/presentation/create_articles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateArticles extends StatefulWidget {
+  const CreateArticles({super.key});
+  
+  @override
+  State<CreateArticles> createState() => _CreateArticlesState();
+}
+
+class _CreateArticlesState extends State<CreateArticles> {
+  final TextEditingController promptController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
