8bd4c91de1d34cc7162844d57d51a3e82bfc77ef
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-06 10:18:46 +0000

    6 Tem 2023 13:18

---
+++ mobile_nft/lib/features/home/presentation/custom_search.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_nft/features/Subscribe/domain/subscribe_arguments.dart';
+
+class CustomSearchDelegate extends SearchDelegate {
+  List<dynamic> searchTerms = [];
+  CustomSearchDelegate(List<dynamic> s) {
+    searchTerms = s;
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<dynamic> matchQuery = [];
+    for (var mag in searchTerms) {
+      if (mag[6].toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(mag);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index][6];
+        List<dynamic> nft = matchQuery[index];
+        return ListTile(
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              Subscribe.routeName,              
+              arguments: SubscribeArguments(nft),
+            );
+          },
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
