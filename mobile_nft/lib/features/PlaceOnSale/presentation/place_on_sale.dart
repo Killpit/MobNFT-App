12192f346670870adc8f69d16a549f7995a2f321
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-07 10:54:23 +0000

    7 Tem 2023 13:54

---
+++ mobile_nft/lib/features/PlaceOnSale/presentation/place_on_sale.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_nft/features/PlaceOnSale/domain/place_on_sale_arguments.dart';
+import 'package:mobile_nft/utils/color.dart';
+import 'package:mobile_nft/utils/fonts.dart';
+import 'package:mobile_nft/utils/func.dart';
+
+class PlaceOnSale extends StatefulWidget {
+  const PlaceOnSale({super.key});
+
+  @override
+  State<PlaceOnSale> createState() => _PlaceOnSaleState();
+
+  static const routeName = "/placeonsale";
+}
+
+class _PlaceOnSaleState extends State<PlaceOnSale> with Func {
+  final TextEditingController priceController = TextEditingController();
+  final List<String> list = ["1 week", "1 month", "3 months", "6 months", "1 year"];
+  String? dropdownValue;
+
+  @override
+  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as PlaceOnSaleArguments;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: brandColor,
+        title: Text(
+          "Place on Sale",
+          style: TextStyle(fontFamily: titleFont),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Set a price",
+              style: TextStyle(
+                fontFamily: bodyFont,
+                fontWeight: FontWeight.w700,
+                fontSize: 16),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: TextField(
+                controller: priceController,
+                decoration: const InputDecoration(
+                  suffixText: 'ETH',
+                )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                "Set a duration",
+                style: TextStyle(
+                  fontFamily: bodyFont,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 16),
+              ),
+            ),
+            
+          ]
+        )
+      )
+    );
+  }
+}
