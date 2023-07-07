1996b38ed20904e2454a9ebcf59a0383a56d7044
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-07 11:59:14 +0000

    7 Tem 2023 14:59

--- mobile_nft/lib/features/PublishingArticle/presentation/publishing_article.dart
+++ mobile_nft/lib/features/PublishingArticle/presentation/publishing_article.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lorem/flutter_lorem.dart';
 import 'package:mobile_nft/utils/color.dart';
 import 'package:mobile_nft/utils/fonts.dart';
 
@@ -20,7 +21,26 @@ class _PublishArticleState extends State<PublishArticle> {
           style: TextStyle(fontFamily: titleFont),
         ),
       ),
-      
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  lorem(paragraphs: 1, words: 7).toUpperCase(),
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: themeColor),
+                ),
+              ),
+            ]
+          )
+        )
+      )
     );
   }
 }
