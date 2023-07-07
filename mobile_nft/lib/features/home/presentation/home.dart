6adc925d016cf98cf5ac81626e48992c6a0e2b6f
Ata Tekeli <tekeliata2000@gmail.com>
2023-07-07 10:34:27 +0000

    7 Tem 2023 13:34

--- mobile_nft/lib/features/home/presentation/home.dart
+++ mobile_nft/lib/features/home/presentation/home.dart
@@ -1,9 +1,12 @@
 
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+import 'package:empty_widget/empty_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_nft/features/Profiles/application/profile_provider.dart';
 import 'package:mobile_nft/features/contracts/application/nft_provider.dart';
 import 'package:mobile_nft/features/home/presentation/custom_search.dart';
 import 'package:mobile_nft/utils/color.dart';
+import 'package:mobile_nft/utils/config.dart';
 import 'package:mobile_nft/utils/constants.dart';
 import 'package:mobile_nft/utils/fonts.dart';
 import 'package:provider/provider.dart';
@@ -34,17 +37,17 @@ class _HomeState extends State<Home> {
         centerTitle: false,
         actions: [
           (currentIndex == 0)
-          ? IconButton(
-            onPressed: () {
-              showSearch(
-                context: context,
-                delegate: CustomSearchDelegate(nft.nfts));
-            },
-            icon: const Icon(Icons.search))
-            : const SizedBox(),
-            Padding(
-              padding: const EdgeInsets.only(right: 5.0),
-              child: ActionChip(
+              ? IconButton(
+                  onPressed: () {
+                    showSearch(
+                        context: context,
+                        delegate: CustomSearchDelegate(nft.nfts));
+                  },
+                  icon: const Icon(Icons.search))
+              : const SizedBox(),
+          Padding(
+            padding: const EdgeInsets.only(right: 5.0),
+            child: ActionChip(
                 onPressed: () {},
                 backgroundColor: themeColor,
                 label: Row(
@@ -53,13 +56,97 @@ class _HomeState extends State<Home> {
                       padding: EdgeInsets.only(right: 5.0),
                       child: Icon(Icons.wallet),
                     ),
-                    
-                  ]
-                )
-              )
-            ) 
-        ]
-      )
+                    RichText(
+                        text: TextSpan(
+                            text: nft.balance.toStringAsFixed(4),
+                            children: const <TextSpan>[
+                          TextSpan(
+                              text: " ETH",
+                              style: TextStyle(
+                                  color: brandColor,
+                                  fontWeight: FontWeight.bold))
+                        ]))
+                  ],
+                )),
+          )
+        ],
+      ),
+      body: (currentIndex == 0)
+          ? (nft.nfts.isNotEmpty)
+              ? GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: (1 / 1.4),
+                    crossAxisCount: 2,
+                  ),
+                  itemCount: nft.nfts.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return NftCard(
+                      source: Source.home,
+                      nft: nft.nfts[index],
+                    );
+                  })
+              : Center(
+                  child: Padding(
+                  padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+                  child: EmptyWidget(
+                    image: null,
+                    packageImage: PackageImage.Image_3,
+                    title: 'No Magazine NFTs',
+                    subTitle:
+                        'There are no available Magazine NFTs. \n Be the first to create one!',
+                    titleTextStyle: const TextStyle(
+                      fontSize: 22,
+                      color: Color(0xff9da9c7),
+                      fontWeight: FontWeight.w500,
+                    ),
+                    subtitleTextStyle: const TextStyle(
+                      fontSize: 14,
+                      color: Color(0xffabb8d6),
+                    ),
+                  ),
+                ))
+          : (currentIndex == 1)
+              ? const CreateNft()
+              : const Profile(),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: currentIndex,
+        showElevation: true,
+        itemCornerRadius: 24,
+        curve: Curves.easeIn,
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            icon: Icon(bottomMenu[0]['icon']),
+            title: Text(bottomMenu[0]['label']),
+            activeColor: brandColor,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(bottomMenu[1]['icon']),
+            title: Text(bottomMenu[1]['label']),
+            activeColor: brandColor,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(bottomMenu[2]['icon']),
+            title: Text(bottomMenu[2]['label']),
+            activeColor: brandColor,
+            textAlign: TextAlign.center,
+          )
+        ],
+        onItemSelected: (index) async {
+          if (index == 0) {
+            nft.getSubscriptions();
+          } else if (index == 2) {
+            status.setProfile(true);
+            nft.getMyProfile();
+            nft.getMyNfts(dummyAddress);
+            nft.getCollectables(dummyAddress);
+          }
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
